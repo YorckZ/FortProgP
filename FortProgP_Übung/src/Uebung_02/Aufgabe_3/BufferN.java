04a215f02ec0cac2c47028ad249e2821b095c1fa
aaronschaper@live.de <tmueLLer+89>
2020-11-13 16:51:08 +0100

    erste Version von Aufgabe 3

--- FortProgP_Übung/src/Uebung_02/Aufgabe_3/BufferN.java
+++ FortProgP_Übung/src/Uebung_02/Aufgabe_3/BufferN.java
@@ -1,33 +1,63 @@
 package Uebung_02.Aufgabe_3;
 public class BufferN<E> {
-	private E[] n;
+	private E[] buffer;
 
-	public BufferN (int capacity) {
-		if (capacity <=0) {
+	public BufferN (int n) {
+		if (n <= 0){
 			throw new IllegalArgumentException();
-			}else {
-				this.n = (E[]) new Object[capacity];
-			}
+		} else {
+			this.buffer = (E[]) new Object[n];
+		}
 	}
 	
-	public synchronized E take() {
+	public synchronized E take() throws InterruptedException {
 		// nimmt ersten Eintrag aus dem Array und löscht diesen; sonst suspendiere
-		return null;
+
+		while(this.isEmpty()){
+			wait();
+		}
+
+		E value = this.buffer[0];
+
+		for (int i = 0; i < this.buffer.length; i++) {
+			if (i == this.buffer.length - 1){
+				this.buffer[i] = null;
+			} else {
+				this.buffer[i] = this.buffer[i + 1];
+			}
+		}
+		notifyAll();
+		return value;
 	}
 	
-	public synchronized void put(E elem) {
+	public synchronized void put(E elem) throws InterruptedException {
 		// Füge Wert hinzu, wenn E[] nicht voll; sonst suspendiere
+		while(this.isFull()){
+			wait();
+		}
+		for (int i = 0; i < this.buffer.length; i++){
+			if(this.buffer[i] == null){
+				this.buffer[i] = elem;
+			}
+		}
+		notifyAll();
 	}
 	
 	public synchronized boolean isEmpty() {
-		// P(E);
-		boolean rv;
-		if (n.length >0) {
-			rv = false;
+
+		if (this.buffer[0] == null) {
+			return true;
 		}else {
-			rv = true;
+			return false;
 		}
-		// V(E)
-		return rv;
+	}
+	
+	public synchronized boolean isFull(){
+		for(E elem : this.buffer){
+			if (elem == null) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
