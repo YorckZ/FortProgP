8de5b80255b3d9e8a8ae4c6b443a730816de9e9f
aaronschaper@live.de <tmueLLer+89>
2020-11-14 18:54:28 +0100

    First implementation of Ü2A3 & Ü2A4

--- src/Uebung_02/Aufgabe_4/Buffer1.java
+++ src/Uebung_02/Aufgabe_4/Buffer1.java
@@ -1,14 +1,15 @@
 package Uebung_02.Aufgabe_4;
+
 import java.util.concurrent.TimeoutException;
 
 /**
-* Single element buffer with synchronization.
-* 
-* @author FortProg team
-* 
-* @param <E>
-*            Type of the element
-*/
+ * Single element buffer with synchronization.
+ *
+ * @author FortProg team
+ *
+ * @param <E>
+ *            Type of the element
+ */
 public class Buffer1<E> {
 
     // element + empty flag
@@ -16,8 +17,8 @@ public class Buffer1<E> {
     private boolean empty;
 
     // synchronization objects
-    private Object r = new Object(); // take, takelong
-    private Object w = new Object(); // put, overwrite
+    private Object r = new Object();
+    private Object w = new Object();
 
     public Buffer1() {
         empty = true;
@@ -29,11 +30,12 @@ public class Buffer1<E> {
     }
 
     /**
-    * take the element from the buffer; suspends on an empty buffer.
-    * 
-    * @return element of the buffer
-    * @throws InterruptedException
-    */
+     * take the element from the buffer; suspends on an empty buffer.
+     *
+     * @return element of the buffer
+     * @throws InterruptedException
+     */
+
     public E take() throws InterruptedException {
         synchronized (r) {
             while (empty) {
@@ -48,12 +50,13 @@ public class Buffer1<E> {
     }
 
     /**
-    * put an element into the buffer; suspends on a full buffer
-    * 
-    * @param o
-    *            Object to put into
-    * @throws InterruptedException
-    */
+     * put an element into the buffer; suspends on a full buffer
+     *
+     * @param o
+     *            Object to put into
+     * @throws InterruptedException
+     */
+
     public void put(E o) throws InterruptedException {
         synchronized (w) {
             while (!empty) {
@@ -68,63 +71,87 @@ public class Buffer1<E> {
     }
 
     /**
-    * Return whether the buffer is empty
-    * 
-    * @return true if empty
-    */
+     * Return whether the buffer is empty
+     *
+     * @return true if empty
+     */
+
     public boolean isEmpty() {
         return empty;
     }
 
     /**
-    * Read the element from the buffer without emptying it; suspends on an
-    * empty buffer.
-    * 
-    * @return element of the buffer
-    * @throws InterruptedException
-    */
-    public E read() throws InterruptedException {
-        // TODO implement this
-                return null;
+     * Read the element from the buffer without emptying it; suspends on an
+     * empty buffer.
+     *
+     * @return element of the buffer
+     * @throws InterruptedException
+     */
+
+    public E read() throws InterruptedException { // TODO: richtig implementiert?
+        synchronized(r) {
+            while (empty) {
+                r.wait();
+            }
+        }
+        return content;
     }
 
     /**
-    * Try to put an element into the buffer; succeeds only for an empty buffer
-    * 
-    * @param elem
-    *            Element to put into
-    * @return true if successful
-    */
-    public boolean tryPut(E elem) {
-        // TODO implement this
-        return false;
+     * Try to put an element into the buffer; succeeds only for an empty buffer
+     *
+     * @param elem
+     *            Element to put into
+     * @return true if successful
+     */
+
+    public synchronized boolean tryPut(E elem) throws InterruptedException { // TODO: richtig implementiert?
+        if(empty){
+            put(elem);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
-    * Overwrite the element in the buffer, even if if the buffer is empty
-    * 
-    * @param elem
-    *            Element to overwrite with
-    */
+     * Overwrite the element in the buffer, even if if the buffer is empty
+     *
+     * @param elem
+     *            Element to overwrite with
+     */
     public void overwrite(E elem) {
-        // TODO implement this
+        content = elem;
     }
 
     /**
-    * take with timeout. The timeout mechanism has to be handcrafted as there
-    * is no way to detect whether a wait() was left because of a timeout or a
-    * notify().
-    * 
-    * @param timeout
-    *            Maximum time to wait in milliseconds
-    * @return
-    * @throws InterruptedException
-    * @throws TimeoutException
-    *             if a timeout occurred
-    */
-    public E take(long timeout) throws InterruptedException, TimeoutException {
-        // TODO implement this
-        return null;
+     * take with timeout. The timeout mechanism has to be handcrafted as there
+     * is no way to detect whether a wait() was left because of a timeout or a
+     * notify().
+     *
+     * @param timeout
+     *            Maximum time to wait in milliseconds
+     * @return
+     * @throws InterruptedException
+     * @throws TimeoutException
+     *             if a timeout occurred
+     */
+
+    public E take(long timeout) throws InterruptedException, TimeoutException { // TODO: richtig implementiert?
+        synchronized (r) {
+            if (empty) {
+                r.wait(timeout);
+            }
+
+            if (!empty) {
+                synchronized (w) {
+                    empty = true;
+                    w.notify();
+                    return content;
+                }
+            } else {
+                throw new TimeoutException();
+            }
+        }
     }
-    
 }
